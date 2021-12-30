1a4814fd94ca29816cb0947005a8a37b69625540
Derek <shouldergiant@icloud.com>
2021-12-30 12:59:15 -0500

    cleanup and set daily scan for new documents when empty

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const RegeneratingDocument = require("./RegeneratingDocument/RegeneratingDocumen
 const RegeneratingTask = require("./models/RegeneratingTask.model");
 const Task = require("./models/Task.model");
 
-//connect to server
-// require("./db");
 
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/pudge-pile";
 
@@ -14,6 +12,7 @@ mongoose
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
 
     const tasks = {};
+    const MSDAILYREFRESH = 1000 * 60 * 60 * 24;
     const refetchThread = {
         id: 0,
         isActive: false,
@@ -23,32 +22,26 @@ mongoose
     
 
     function start() {
-        console.log("boop!")
         RegeneratingTask.find().select(["-_id", "text", "regenInterval", "scheduleDate"]).then(taskData => {
             if (!refetchThread.isActive && taskData.length === 0) {
-                refetchThread.id = setInterval(start, 1000);
+                refetchThread.id = setInterval(start, MSDAILYREFRESH);
                 refetchThread.isActive = true;
             }
             else for (const data of taskData) {
                 const { text, regenInterval, scheduleDate } = data;
-                console.log("Creating Task", data);
                 tasks[text] = new RegeneratingDocument({ text }, regenInterval, regenerate, scheduleDate);
             }
         });
     }
-    //TODO: load application state, build queues, gather and attempt creating overdue documents
     
     async function regenerate({ text }) {
-        //check if there are new documents
-            console.log("Regenerate", { text});
         const taskList = await RegeneratingTask.find().select(["-_id", "text", "regenInterval", "scheduleDate"]);
         if (taskList.length === 0) { 
             if (!refetchThread.isActive) {
-                refetchThread.id = setInterval(start, 1000);
+                refetchThread.id = setInterval(start, MSDAILYREFRESH);
                 refetchThread.isActive = true;
             }
             removeDocument(text);
-            console.log("Terminated because empty task list");
         }
         else {
             if(refetchThread.isActive) {
@@ -57,20 +50,12 @@ mongoose
             }
             let found = false;
             for(const task of taskList) {
-                console.log(task.text, text);
                 if (!tasks[task.text]) {
-                    console.log("New task discovered", tasks[task.text])
                     tasks[task.text] = new RegeneratingDocument({ text: task.text }, task.regenInterval, regenerate, task.scheduleDate);
                 }
                 if (task.text === text) found = true;
             }
-            if (!found){ 
-                removeDocument(text);
-                console.log("Terminated because task deleted");
-            }
-            else {
-                console.log("Updating Task", text);
-            }
+            found ? Task.findOneAndUpdate({text}, {text}, {upsert: true}).exec() : removeDocument(text);
         }
     }
     
@@ -79,16 +64,6 @@ mongoose
         delete tasks[id];
     }
   })
-  // .then((result) => {
-  //   console.log("closing");
-  //   mongoose.connection.close();
-  // })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
-
-
-// console.log("ping");
-// const test = new RegeneratingDocument({ duck: "quack" }, 2000, (document) => {
-//     console.log(document.duck);
-// }, Date.now() + 1000);
