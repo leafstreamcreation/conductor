c8f92dfa157968865324add93e2019d413ea824e
Derek <shouldergiant@icloud.com>
2022-01-01 11:27:01 -0500

    Added document update handling

--- RegeneratingDocument/RegeneratingDocument.js
+++ RegeneratingDocument/RegeneratingDocument.js
@@ -1,19 +1,48 @@
 module.exports = class RegeneratingDocument {
-    constructor(document, regenFrequency, action, nextRegenTime = Date.now() + regenFrequency) {
+    constructor(document, regenInterval, action, nextRegenTime = Date.now() + regenInterval) {
         this.document = document;
-        this.regenFrequency = regenFrequency;
+        this.regenInterval = regenInterval;
         this.action = action;
         this.nextRegenTime = nextRegenTime;
 
+        let nextFiringTime = nextRegenTime;
+
         if(Date.now() > nextRegenTime) {
             const timeOverdue = Date.now() - nextRegenTime;
-            this.nextRegenTime = Date.now() + regenFrequency - (timeOverdue % regenFrequency);
+            nextFiringTime = Date.now() + regenInterval - (timeOverdue % regenInterval);
         }
         setTimeout(() => {
             action(document);
-            this.id = setInterval(action, regenFrequency, document);
-        }, this.nextRegenTime - Date.now());
+            this.id = setInterval(action, regenInterval, document);
+        }, nextFiringTime - Date.now());
     }
+    
+    updateWith(regenInterval, nextRegenTime) {
+        // console.log("updating");
+        this.regenInterval = regenInterval;
+        this.nextRegenTime = nextRegenTime;
+        clearInterval(this.id);
+
+        //get next start interval
+        let nextFiringTime = nextRegenTime;
+
+        if(Date.now() > nextRegenTime) {
+            const timeOverdue = Date.now() - nextRegenTime;
+            nextFiringTime = Date.now() + regenInterval - (timeOverdue % regenInterval);
+        }
+        //set timeout to start interval
+        setTimeout(() => {
+            this.action(this.document);
+            this.id = setInterval(this.action, regenInterval, this.document);
+        }, nextFiringTime - Date.now());
+    }
+
+    hasData(int, time) {
+        if (int != this.regenInterval) return false;
+        if (time.valueOf() !== this.nextRegenTime.valueOf()) return false;
+        return true;
+    }
+
     terminate() {
         clearInterval(this.id);
     }
