7e260d7553551776f9298b46b1768ae4c69fdc1d
Derek <shouldergiant@icloud.com>
2022-01-10 14:56:00 -0500

    Timing and Scheduling unit tests for RegeneratingDocuments; Added general testing foundation

---
+++ tests/RegeneratingDocument.test.js
@@ -0,0 +1,62 @@
+const Utilities = require("./TestUtilities");
+const RegeneratingDocument = require("../RegeneratingDocument/RegeneratingDocument");
+
+//regendocuments have a document, interval, action, and nextRegenTime
+
+//no db connection required here
+
+describe("RegenDocument tests", () => {
+    test("action fires", async () => {
+        let actionFired = false;
+        const doc = new RegeneratingDocument({}, 2, () => {
+            actionFired = true;
+        });
+        await new Promise((resolve) => {setTimeout(resolve, 5)});
+        expect(actionFired).toBe(true);
+        doc.terminate();
+    });
+
+    test("action fires after regenInterval when scheduled immediately", async () => {
+        const REGENTIME = 1000;
+        const startTime = Date.now();
+        let actualTime = 0;
+        const doc = new RegeneratingDocument({}, REGENTIME, () => {
+            actualTime = Date.now() - startTime;
+        });
+        await new Promise((resolve) => {setTimeout(resolve, REGENTIME + 5)});
+        const deviation = Math.abs(1 - (actualTime / REGENTIME));
+        expect(deviation).toBeCloseTo(0.0, 2);
+        doc.terminate();
+    });
+
+    test("action fires at schedule date", async () => {
+        const SCHEDULE_DATE = Date.now() + 1500;
+        const REGENTIME = 1000;
+        let firingDate = 0;
+        const doc = new RegeneratingDocument({}, REGENTIME, () => {
+            firingDate = Date.now();
+        }, SCHEDULE_DATE);
+        await new Promise((resolve) => {setTimeout(resolve, 1490)});
+        expect(firingDate).toBe(0);
+        await new Promise((resolve) => {setTimeout(resolve, 20)});
+        const deviation = Math.abs(1 - (firingDate / (SCHEDULE_DATE)));
+        expect(deviation).toBeCloseTo(0.0, 2);
+        doc.terminate();
+    });
+
+    test("action fires in time with regenInterval starting from schedule date", async () => {
+        const SCHEDULE_DATE = Date.now() + 1500;
+        const REGENTIME = 1000;
+        let firingDate = 0;
+        const doc = new RegeneratingDocument({}, REGENTIME, () => {
+            firingDate = Date.now();
+        }, SCHEDULE_DATE);
+        await new Promise((resolve) => {setTimeout(resolve, 1490)});
+        expect(firingDate).toBe(0);
+        await new Promise((resolve) => {setTimeout(resolve, 20)});
+        const deviation = Math.abs(1 - (firingDate / (SCHEDULE_DATE)));
+        expect(deviation).toBeCloseTo(0.0, 2);
+        doc.terminate();
+    });
+    
+});
